b0e9d03366a263147c81098c1f77ff94e53762b2
Chris <Chris@Chriss-MacBook-Pro.local>
2016-06-22 15:44:21 -0500

    adding simplesimon changes

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -1,9 +1,162 @@
 'use strict'
-$(document).ready(function() {
-	$('#box1').click(function(){
-		console.log(im clicking)
-		$('#animate-box').animate({
-			opacity: '0'
-		})
-	})
-}); 
+	
+	var userSequence = [];
+	var gameSequence = [];
+	var homerSound = $('#homer');
+	var barneySound = $('#barney');
+	var duffSound = $('#beer');
+	var bartSound = $('#bart');
+	var boxes = $('.col-md-6');
+
+		function start() {
+			gameSequence = [];
+			goSimon();
+		}
+
+		function goSimon() {
+			userSequence = [];
+			addRandomBoxToSequence();
+			playback()
+		}
+
+		function addRandomBoxToSequence() {
+			var random = Math.floor(Math.random() * 4);
+			gameSequence.push(boxes[random].class);
+		}
+
+		function playback() {
+
+			endInput();
+
+		document.getElementById('round').innerText = 'Round: ' + gameSequence.length;
+
+		var i = 0;
+
+			var intervalId = setInterval(function (){
+
+					lightUp(document.getElementById(gameSequence[i]));
+
+					i++;
+
+				if (i >= gameSequence.length) {
+
+					clearInterval(intervalId);
+
+					beginInput();
+				}
+
+			}, 1000);
+		}	
+
+	    function lightUp(element) {
+	            //element.style.opacity = "1";
+	            var fadeoutTimerId = setTimeout(function() {
+	                //element.style.opacity = "0.3";
+	            }, 100);
+	        
+			$(document).ready(function() {
+					$('#box1').click(function(){
+						// console.log('im clicking');
+						$('#box1').animate({
+							opacity: '1'
+						}, 150).animate({
+							opacity: '.3'
+						})
+						duffSound.trigger('play');
+					})
+				$(document).ready(function() {
+					$('#box2').click(function(){
+						
+						$('#box2').animate({
+							opacity: '1'
+						}, 150).animate({
+							opacity: '.3'
+						})
+						bartSound.trigger('play');
+					})
+				})
+				$(document).ready(function() {
+					$('#box3').click(function(){
+						// console.log('im clicking');
+						$('#box3').animate({
+							opacity: '1'
+						}, 150).animate({
+							opacity: '.3'
+						})
+						homerSound.trigger('play');
+					})
+				})
+				$(document).ready(function() {
+					$('#box4').click(function(){
+						// console.log('im clicking');
+						$('#box4').animate({
+							opacity: '1'
+						}, 150).animate({
+							opacity: '.3'
+						})
+						barneySound.trigger('play');
+					})
+				})							
+			})
+		}
+
+
+        function compareSequences() {
+	            var sequenceError = false;
+	            // loop through the input sequence and perform the following:
+	            // check if the game sequence and input sequence match for a given offset
+	            // if they don't match, set your error status variable to true and get out of the loop
+	            for (var i = 0; i < userSequence.length; i++) {
+	              if (gameSequence[i] == undefined || gameSequence[i] != userSequence[i]) {
+	                sequenceError = true;
+	                break;
+	              }
+	            }
+	           // check if there is an error
+	            // if there is, call gameOver()
+	            // otherwise, check if the input sequence is the same length as the game sequence
+		            // if it is, the user has completed the sequence, so clear the input sequence and call simonMove()
+		    if (sequenceError) {
+		              gameOver();
+		        } else if (userSequence.length == gameSequence.length) {
+		              goSimon();
+	        }
+	    }
+
+        function gameOver() {
+            // hard reload the page.
+            location.reload(true);
+            confirm("Game over. Play again?");
+            if(confirm) {
+                start();
+            }
+        }
+
+        function stop() {
+            alert('User stopped the game');
+            location.reload(true);
+        }
+        function userClick() {
+            var userChoice = this.id;
+            lightUp(this);
+            userSequence.push(userChoice);
+            compareSequences();
+        }
+
+
+		function beginInput () {
+			document.getElementById('box1').addEventListener('click',userClick, false);
+			document.getElementById('box2').addEventListener('click',userClick, false);
+			document.getElementById('box3').addEventListener('click',userClick, false);
+			document.getElementById('box4').addEventListener('click',userClick, false);
+		}
+		function endInput () {
+			document.getElementById('box1').removeEventListener('click',userClick, false);
+			document.getElementById('box2').removeEventListener('click',userClick, false);
+			document.getElementById('box3').removeEventListener('click',userClick, false);
+			document.getElementById('box4').removeEventListener('click',userClick, false);		
+		}
+
+		document.getElementById('start').addEventListener('click', start, false);
+		document.getElementById('stop').addEventListener('click', stop, false);
+		
